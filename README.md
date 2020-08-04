3775c1cc0e793fec92317f1250097a9f8e5468af
Abhinn Vyas <abhinnvyas2@gmail.com>
2020-08-04 02:39:07 -0700

    first Commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Python-Calculator-CLI
 This is a Simple CLI calculator with Python.
+
+* You must have python installed in your system. If you dont have python installed then install it from its official website https://www.python.org/ 
+
+# How to Use ?
+Clone this Repository by running the following command in the terminal.
+
+```git clone https://github.com/abhinnvyas/Python-Calculator-CLI.git ```
+
+After the navigate to that directory.
+
+```cd Python-Calculator-CLI```
+
+Run the Script by the simply writing the following command.
+
+```python main.py```
+
+Then Simply run the below Command to show all the options.
+
+```python main.py -h```
+
+*And now i know you all are smarty pants. You all will figure it out.*
---
+++ main.py
@@ -0,0 +1,39 @@
+import argparse
+
+def add(args):
+    return args[0]+args[1]
+
+def multiply(args):
+    return args[0]*args[1]
+
+def divide(args):
+    return args[0]/args[1]
+
+def subtract(args):
+	return args[0]-args[1]
+
+parser = argparse.ArgumentParser("A Command Line Calculator."+
+                                 "\n\tAuthor - Abhinn Vyas")
+
+parser.add_argument('-a','--add',nargs=2,metavar=('num1','num2'),
+                    help="num1 added to num2",type=int)
+parser.add_argument('-m','--mult',nargs=2,metavar=('num1','num2'),
+                    help="num1 multiplied to num2",type=int)
+parser.add_argument('-d','--div',nargs=2,metavar=('num1','num2'),
+                    help="num1 divided by num2",type=int)
+parser.add_argument('-s','--sub',nargs=2,metavar=('num1','num2'),
+                    help="num2 subtracted from num1",type=int)
+
+arg = parser.parse_args()
+
+if arg.add != None:
+    print(method.add(arg.add))
+elif arg.mult != None:
+    print(method.multiply(arg.mult))
+elif arg.div != None:
+    print(method.divide(arg.div))
+elif arg.sub != None:
+    print(method.subtract(arg.sub))
+else:
+    print("Error: No Argument specified")
+
